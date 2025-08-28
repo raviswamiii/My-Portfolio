6eac0d85ce195893661b71cc4b25ababb7984374
raviswamiii <raviswamisamsung@gmail.com>
2025-08-29 00:13:32 +0530

    home3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,26 +1,10 @@
-import React from "react";
-import { Home } from "./pages/Home";
-import { Canvas } from "@react-three/fiber";
-import { Model } from "./components/Model";
-import { OrbitControls } from "@react-three/drei";
+import React from 'react'
+import { Home } from './pages/Home'
 
 export const App = () => {
   return (
-    <div className="bg-black h-screen flex justify-center items-center">
-      <div className="bg-black flex shadow-[0_0_20px_#fff] h-[90%] w-[95%] rounded-4xl overflow-hidden">
-        <div className="h-full w-full">
-          <Canvas>
-            <Model />
-            <OrbitControls
-              enableZoom={false}
-              autoRotate
-              autoRotateSpeed={0.5}
-            />
-          </Canvas>
-        </div>
-
-        <Home/>
-      </div>
+    <div>
+      <Home />
     </div>
-  );
-};
+  )
+}
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,21 +1,46 @@
 import React from "react";
+import { Model } from "../components/Model";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export const Home = () => {
   return (
-    <div className="absolute w-1/2">
-      <div className=" flex ml-5 mt-4">
-        <h1 className="text-white orbitron text-sm">.raviswamiii</h1>
-      </div>
+    <div className="bg-black h-screen flex justify-center items-center">
+      <div className="relative bg-black flex justify-center shadow-[0_0_20px_#fff] h-[90%] w-[95%] rounded-4xl overflow-hidden">
+        <div className="h-full w-full">
+          <Canvas>
+            <Model />
+            <OrbitControls
+              enableZoom={false}
+              autoRotate
+              autoRotateSpeed={0.5}
+            />
+          </Canvas>
+        </div>
 
-      <div className="text-white pt-40 pl-35 flex flex-col bg-transparent">
-        <h1 className="text-4xl orbitron mb-2">Hi, I’m Ravi Swami,</h1>
-        <h2 className="text-5xl mb-2 orbitron">A Web Developer</h2>
-        <h3>
-          A dedicated MERN Stack Developer, specializing in building web
-          applications. I deliver high-quality solutions, modern design and
-          reliable performance, ensuring the best services tailored to your
-          requirements.
-        </h3>
+        <div className="absolute left-6 my-4">
+          <h1 className="text-white orbitron text-sm">.raviswamiii</h1>
+        </div>
+
+          <div className="absolute flex mx-5 my-4 text-white orbitron gap-16 text-sm ">
+            <p className="underline">Home</p>
+            <p className="underline">Skills</p>
+            <p className="underline">Projects</p>
+            <p className="underline">Contact Me</p>
+          </div>
+
+        <div className="text-white pl-35 pt-48 flex flex-col absolute left-0 w-1/2">
+          <h1 className="text-4xl orbitron mb-2">Hi, I’m Ravi Swami,</h1>
+          <h2 className="text-5xl mb-2 orbitron">A Web Developer</h2>
+          <h3>
+            A dedicated MERN Stack Developer, specializing in building web
+            applications. I deliver high-quality solutions, modern design and
+            reliable performance, ensuring the best services tailored to your
+            requirements.
+          </h3>
+        </div>
+
+        
       </div>
     </div>
   );
